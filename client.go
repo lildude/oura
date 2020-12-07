30b5e5c611d5aeda8ef4e43b8dec28cd2e9f09d7
Colin Seymour <colin@symr.io>
2020-12-07 12:30:06 +0000

    Add client with tests

---
+++ client.go
@@ -0,0 +1,171 @@
+package oura
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	// BaseURLV1 is Oura's v1 API endpoint
+	BaseURLV1 = "https://api.ouraring.com/v1/"
+	userAgent = "go-oura"
+)
+
+// ClientOptions is a set of options that can be specified when creating a Oura client
+type ClientOptions struct {
+	BaseURL *url.URL
+}
+
+// Client holds configuration items for the Oura client and provides methods that interact with the Oura API.
+type Client struct {
+	baseURL *url.URL
+
+	userAgent string
+	client    *http.Client
+}
+
+// NewClient returns a new Oura API client. If a nil httpClient is
+// provided, http.DefaultClient will be used. To use API methods which require
+// authentication, provide an http.Client that will perform the authentication
+// for you (such as that provided by the golang.org/x/oauth2 library).
+// Inspiration: https://github.com/google/go-github/blob/master/github/github.go
+func NewClient(cc *http.Client) *Client {
+	if cc == nil {
+		cc = http.DefaultClient
+	}
+	baseURL, _ := url.Parse(BaseURLV1)
+
+	c := &Client{baseURL: baseURL, userAgent: userAgent, client: cc}
+	return c
+}
+
+// NewClientWithOptions takes ClientOptions, configures and returns a new client.
+func NewClientWithOptions(cc *http.Client, opts ClientOptions) *Client {
+	c := NewClient(cc)
+	c.baseURL = opts.BaseURL
+	return c
+}
+
+// NewRequest creates an HTTP Request. The client baseURL is checked to confirm that it has a trailing
+// slash. A relative URL should be provided without the leading slash. If a non-nil body is provided
+// it will be JSON encoded and included in the request.
+// Inspiration: https://github.com/google/go-github/blob/master/github/github.go
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	if strings.HasSuffix(c.baseURL.Path, "/") == false {
+		return nil, fmt.Errorf("client baseURL does not have a trailing slash: %q", c.baseURL)
+	}
+
+	u, err := c.baseURL.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		enc := json.NewEncoder(buf)
+		enc.SetEscapeHTML(false)
+		err := enc.Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	req.Header.Set("Accept", "application/json")
+	if c.userAgent != "" {
+		req.Header.Set("User-Agent", c.userAgent)
+	}
+	return req, nil
+}
+
+// Do sends a request and returns the response. An error is returned if the request cannot
+// be sent or if the API returns an error. If a response is received, the body response body
+// is decoded and stored in the value pointed to by v.
+// Inspiration: https://github.com/google/go-github/blob/master/github/github.go
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*http.Response, error) {
+	req = req.WithContext(ctx)
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			return nil, errors.Wrap(err, ctx.Err().Error())
+		default:
+			return nil, err
+		}
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to read body")
+	}
+	resp.Body.Close()
+
+	// Anything other than a HTTP 2xx response code is treated as an error. But the structure of error
+	// responses differs depending on the API being called. Some APIs return validation errors as part
+	// of the standard response. Others respond with a standardised error structure.
+	if c := resp.StatusCode; c >= 300 {
+
+		// Handle auth errors
+		if resp.StatusCode == http.StatusUnauthorized || resp.StatusCode == http.StatusForbidden {
+			err := AuthError(http.StatusText(resp.StatusCode))
+			return resp, err
+		}
+
+		// Try parsing the response using the standard error schema. If this fails we wrap the parsing
+		// error and return. Otherwise return the errors included in the API response payload.
+		var e = Errors{}
+		err := json.Unmarshal(data, &e)
+		if err != nil {
+			err = errors.Wrap(err, http.StatusText(resp.StatusCode))
+			return resp, errors.Wrap(err, "unable to parse API error response")
+		}
+
+		if len(e) != 0 {
+			return resp, errors.Wrap(e, http.StatusText(resp.StatusCode))
+		}
+
+		// In some cases, the error response is returned as part of the
+		// requested resource. In these cases we attempt to decode the
+		// resource and return the error.
+		err = json.Unmarshal(data, v)
+		if err != nil {
+			err = errors.Wrap(err, http.StatusText(resp.StatusCode))
+			return resp, errors.Wrap(err, "unable to parse API response")
+		}
+
+		err = errors.New("no additional error information available")
+		return resp, errors.Wrap(err, http.StatusText(resp.StatusCode))
+	}
+
+	if v != nil && len(data) != 0 {
+		err = json.Unmarshal(data, v)
+
+		switch err {
+		case nil:
+		case io.EOF:
+			err = nil
+		default:
+			err = errors.Wrap(err, "unable to parse API response")
+		}
+	}
+
+	return resp, err
+}
---
+++ client_test.go
@@ -0,0 +1,239 @@
+package oura
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestNewClient confirms that a client can be created with the default baseURL
+// and default User-Agent.
+func TestNewClient(t *testing.T) {
+	c := NewClient(nil)
+
+	assert.Equal(t, BaseURLV1, c.baseURL.String(), "should configure the client to use the default url")
+	assert.Equal(t, userAgent, c.userAgent, "should configure the client to use the default user-agent")
+}
+
+// TestNewClientWithOptions confirms that a new client can be created by passing
+// in custom ClientOptions.
+func TestNewClientWithOptions(t *testing.T) {
+	baseURL, _ := url.Parse("https://dummyurl:4000")
+	opts := ClientOptions{
+		BaseURL: baseURL,
+	}
+	c := NewClientWithOptions(nil, opts)
+
+	assert.Equal(t, baseURL.String(), c.baseURL.String(), "should configure the client to use the custom url")
+}
+
+// TestNewRequest confirms that NewRequest returns an API request with the
+// correct URL, a correctly encoded body and the correct User-Agent and
+// Content-Type headers set.
+func TestNewRequest(t *testing.T) {
+	c := NewClient(nil)
+
+	t.Run("valid request", func(tc *testing.T) {
+
+		inURL, outURL := "foo", BaseURLV1+"foo"
+		inBody, outBody := &UserInfo{Age: 99, Weight: 102, Gender: "ano", Email: "user@example.com"}, `{"age":99,"weight":102,"gender":"ano","email":"user@example.com"}`+"\n"
+
+		req, err := c.NewRequest("GET", inURL, inBody)
+
+		assert.Nil(tc, err, "should not return an error")
+		assert.Equal(tc, outURL, req.URL.String(), "should expand relative URLs to absolute URLs")
+
+		body, _ := ioutil.ReadAll(req.Body)
+		assert.Equal(tc, outBody, string(body), "should encode the request body as JSON")
+		assert.Equal(tc, c.userAgent, req.Header.Get("User-Agent"), "should pass the correct user agent")
+		assert.Equal(tc, "application/json", req.Header.Get("Content-Type"), "should set the content-type as application/json")
+	})
+
+	t.Run("request with invalid JSON", func(tc *testing.T) {
+		type T struct{ A map[interface{}]interface{} }
+		_, err := c.NewRequest("GET", ".", &T{})
+		assert.Error(tc, err, "should return an error")
+	})
+
+	t.Run("request with an invalid URL", func(tc *testing.T) {
+		_, err := c.NewRequest("GET", ":", nil)
+		assert.Error(tc, err, "should return an error")
+	})
+
+	t.Run("request with an invalid base path", func(tc *testing.T) {
+		u, _ := url.Parse("http://test.local")
+		o := ClientOptions{BaseURL: u}
+		lc := NewClientWithOptions(nil, o)
+
+		_, err := lc.NewRequest("GET", "/", nil)
+		assert.Error(tc, err, "should return an error")
+	})
+
+	t.Run("request with an invalid Method", func(tc *testing.T) {
+		_, err := c.NewRequest("\n", "/", nil)
+		assert.Error(tc, err, "should return an error")
+	})
+
+	t.Run("request with an empty body", func(tc *testing.T) {
+		req, err := c.NewRequest("GET", ".", nil)
+		assert.Nil(tc, err, "should not return an error")
+		assert.Nil(tc, req.Body, "should return an empty body")
+	})
+
+}
+
+// TestDo confirms that Do returns a JSON decoded value when making a request. It
+// confirms the correct verb was used and that the decoded response value matches
+// the expected result.
+func TestDo(t *testing.T) {
+	t.Run("successful GET request", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		type foo struct{ A string }
+
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, "GET", r.Method)
+			fmt.Fprint(w, `{"A":"a"}`)
+		})
+
+		want := &foo{"a"}
+		got := new(foo)
+
+		req, _ := client.NewRequest("GET", ".", nil)
+		client.Do(context.Background(), req, got)
+
+		assert.ObjectsAreEqual(want, got)
+	})
+
+	t.Run("GET request that returns an HTTP error", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			w.WriteHeader(http.StatusInternalServerError)
+		})
+
+		req, _ := client.NewRequest("GET", ".", nil)
+		resp, err := client.Do(context.Background(), req, nil)
+
+		assert.Equal(tc, http.StatusInternalServerError, resp.StatusCode)
+		assert.Error(tc, err, "should return an error")
+	})
+
+	t.Run("GET request that receives an empty payload", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		type foo struct{ A string }
+
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, _ := client.NewRequest("GET", ".", nil)
+		got := new(foo)
+		resp, err := client.Do(context.Background(), req, got)
+
+		assert.Equal(tc, http.StatusOK, resp.StatusCode)
+		assert.Nil(tc, err, "should not return an error")
+	})
+
+	t.Run("GET request that receives an HTML response", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		type foo struct{ A string }
+
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			w.WriteHeader(http.StatusOK)
+			html := `<!doctype html>
+			<html lang="en-GB">
+			<head>
+			  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+			  <title>Default Page Title</title>
+			  <link rel="shortcut icon" href="favicon.ico">
+			  <link rel="icon" href="favicon.ico">
+			  <link rel="stylesheet" type="text/css" href="styles.css">
+			</head>
+
+			<body>
+
+			</body>
+			</html>	`
+			fmt.Fprintln(w, html)
+		})
+
+		req, _ := client.NewRequest("GET", ".", nil)
+		got := new(foo)
+		resp, err := client.Do(context.Background(), req, got)
+
+		assert.Equal(tc, http.StatusOK, resp.StatusCode)
+		assert.Error(tc, err, "should return an error")
+	})
+
+	t.Run("request on a cancelled context", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, _ := client.NewRequest("GET", ".", nil)
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+		resp, err := client.Do(ctx, req, nil)
+
+		assert.Error(tc, err, "should return an error")
+		assert.Nil(tc, resp, "should not return a response")
+	})
+
+	t.Run("GET request that returns a forbidden response", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			w.WriteHeader(http.StatusForbidden)
+		})
+
+		req, _ := client.NewRequest("GET", ".", nil)
+		resp, err := client.Do(context.Background(), req, nil)
+
+		assert.Equal(tc, http.StatusForbidden, resp.StatusCode)
+		assert.Error(tc, err, "should return an error")
+		if _, ok := err.(AuthError); ok == false {
+			t.Errorf("should return a starling.AuthError: %T", err)
+		}
+		if err, ok := err.(Error); ok == true && err.Temporary() == true {
+			t.Errorf("should not return a temporary error")
+		}
+
+	})
+
+}
+
+// Setup establishes a test Server that can be used to provide mock responses during testing.
+// It returns a pointer to a client, a mux, the server URL and a teardown function that
+// must be called when testing is complete.
+func setup() (client *Client, mux *http.ServeMux, serverURL string, teardown func()) {
+	mux = http.NewServeMux()
+	server := httptest.NewServer(mux)
+
+	c := NewClient(nil)
+	url, _ := url.Parse(server.URL + "/")
+	c.baseURL = url
+
+	return c, mux, server.URL, server.Close
+}
