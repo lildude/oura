dc35db0c00f24031767097f272116f4715e4ef60
Colin Seymour <colin@symr.io>
2020-12-07 18:05:25 +0000

    Add support for querying bedtimes

---
+++ bedtime.go
@@ -0,0 +1,55 @@
+package oura
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// Bedtime represents a single bedtime recommendation
+type Bedtime struct {
+	Date          	string 											`json:"date"`
+	BedtimeWindow 	struct {
+										Start int `json:"start"`
+										End   int `json:"end"`
+									}														`json:"bedtime_window"`
+	Status 					string 											`json:"status"`
+}
+
+// IdealBedtimes represents all ideal bedtimes for the period requested
+type IdealBedtimes struct {
+	IdealBedtimes		[]Bedtime										`json:"ideal_bedtimes"`
+}
+
+// Bedtime gets all of the ideal bedtimes for a specified period of time.
+// If a start and end date are not provided, ie are empty strings, we fall back to Oura which states:
+// 	"If you omit the start date, it will be set to one week ago.
+//	 If you omit the end date, it will be set to the current day."
+func (c *Client) Bedtime(ctx context.Context, start string, end string) (*IdealBedtimes, *http.Response, error) {
+	path := "bedtime"
+	params := url.Values{}
+
+	if start != "" {
+		params.Add("start", start)
+	}
+	if end != "" {
+		params.Add("end", end)
+	}
+	if len(params) > 0 {
+		path += fmt.Sprintf("?%s", params.Encode())
+	}
+
+	req, err := c.NewRequest("GET", path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var bedtimes *IdealBedtimes
+	resp, err := c.Do(ctx, req, &bedtimes)
+	if err != nil {
+		return bedtimes, resp, err
+	}
+
+	return bedtimes, resp, nil
+}
---
+++ bedtime_test.go
@@ -0,0 +1,80 @@
+package oura
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBedtime(t *testing.T) {
+	mock := `{
+		"ideal_bedtimes": [
+			{
+				"date": "2020-03-17",
+				"bedtime_window": {
+					"start": -3600,
+					"end": 0
+				},
+				"status": "IDEAL_BEDTIME_AVAILABLE"
+			},
+			{
+				"date": "2020-03-18",
+				"bedtime_window": {
+					"start": null,
+					"end": null
+				},
+				"status": "LOW_SLEEP_SCORES"
+			}
+		]
+	}`
+
+	t.Run("get bedtime without specific dates", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		mux.HandleFunc("/bedtime", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			fmt.Fprint(w, mock)
+		})
+
+		got, _, err := client.Bedtime(context.Background(), "", "")
+		assert.NoError(t, err, "should not return an error")
+
+		want := &IdealBedtimes{}
+		json.Unmarshal([]byte(mock), want)
+
+		assert.ObjectsAreEqual(want, got)
+	})
+
+	t.Run("get bedtime with only start date", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		mux.HandleFunc("/bedtime", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			assert.Equal(tc, "/bedtime?start=2020-01-20", r.URL.String())
+			fmt.Fprint(w, mock)
+		})
+
+		_, _, err := client.Bedtime(context.Background(), "2020-01-20", "")
+		assert.NoError(t, err, "should not return an error")
+	})
+
+	t.Run("get bedtime with start and end dates", func(tc *testing.T) {
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		mux.HandleFunc("/bedtime", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(tc, http.MethodGet, r.Method)
+			assert.Equal(tc, "/bedtime?end=2020-01-25&start=2020-01-20", r.URL.String())
+			fmt.Fprint(w, mock)
+		})
+
+		_, _, err := client.Bedtime(context.Background(), "2020-01-20", "2020-01-25")
+		assert.NoError(t, err, "should not return an error")
+	})
+}
