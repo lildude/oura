625ef8ab0330034e4b973d83cf47ae551a2e1d3d
Colin Seymour <colin@symr.io>
2020-12-10 17:54:01 +0000

    Use test cases for bedtime tests

--- bedtime_test.go
+++ bedtime_test.go
@@ -10,71 +10,104 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBedtime(t *testing.T) {
-	mock := `{
-		"ideal_bedtimes": [
-			{
-				"date": "2020-03-17",
-				"bedtime_window": {
-					"start": -3600,
-					"end": 0
+var bedtimeTestCases = []struct {
+	name        string
+	start       string
+	end         string
+	expectedURL string
+	mock        string
+}{
+	{
+		name: "get bedtime without specific dates",
+		start: "",
+		end: "",
+		expectedURL: "",
+		mock: `{
+			"ideal_bedtimes": [
+				{
+					"date": "2020-03-17",
+					"bedtime_window": {
+						"start": -3600,
+						"end": 0
+					},
+					"status": "IDEAL_BEDTIME_AVAILABLE"
 				},
-				"status": "IDEAL_BEDTIME_AVAILABLE"
-			},
-			{
-				"date": "2020-03-18",
-				"bedtime_window": {
-					"start": null,
-					"end": null
+				{
+					"date": "2020-03-18",
+					"bedtime_window": {
+						"start": null,
+						"end": null
+					},
+					"status": "LOW_SLEEP_SCORES"
+				}
+			]
+		}`,
+	},
+	{
+		name: "get bedtime with only start date",
+		start: "2020-01-20",
+		end: "",
+		expectedURL: "/bedtime?start=2020-01-20",
+		mock: `{
+			"ideal_bedtimes": [
+				{
+					"date": "2020-01-20",
+					"status": "IDEAL_BEDTIME_AVAILABLE"
 				},
-				"status": "LOW_SLEEP_SCORES"
-			}
-		]
-	}`
+				{
+					"date": "2020-01-21",
+					"status": "LOW_SLEEP_SCORES"
+				},
+				{
+					"date": "2020-01-22",
+					"status": "IDEAL_BEDTIME_AVAILABLE"
+				},
+				{
+					"date": "2020-01-23",
+					"status": "LOW_SLEEP_SCORES"
+				},
+			]
+		}`,
+	},
+	{
+		name: "get bedtime with start and end dates",
+		start: "2020-01-20",
+		end: "2020-01-22",
+		expectedURL: "/bedtime?end=2020-01-22&start=2020-01-20",
+		mock: `{
+			"ideal_bedtimes": [
+				{
+					"date": "2020-01-20",
+					"status": "IDEAL_BEDTIME_AVAILABLE"
+				},
+				{
+					"date": "2020-01-21",
+					"status": "LOW_SLEEP_SCORES"
+				},
+				{
+					"date": "2020-01-22",
+					"status": "IDEAL_BEDTIME_AVAILABLE"
+				}
+			]
+		}`,
+	},
+}
 
-	t.Run("get bedtime without specific dates", func(tc *testing.T) {
-		client, mux, _, teardown := setup()
-		defer teardown()
+func testGetBedtime(t *testing.T, start, end, expectedURL, mock string) {
+	client, mux, _, teardown := setup()
+	defer teardown()
 
-		mux.HandleFunc("/bedtime", func(w http.ResponseWriter, r *http.Request) {
-			assert.Equal(tc, http.MethodGet, r.Method)
-			fmt.Fprint(w, mock)
-		})
-
-		got, _, err := client.GetBedtime(context.Background(), "", "")
-		assert.NoError(t, err, "should not return an error")
-
-		want := &IdealBedtimes{}
-		json.Unmarshal([]byte(mock), want)
-
-		assert.ObjectsAreEqual(want, got)
+	mux.HandleFunc("/bedtime", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, expectedURL, r.URL.String())
+		fmt.Fprint(w, mock)
 	})
 
-	t.Run("get bedtime with only start date", func(tc *testing.T) {
-		client, mux, _, teardown := setup()
-		defer teardown()
+	got, _, err := client.GetBedtime(context.Background(), start, end)
+	assert.NoError(t, err, "should not return an error")
 
-		mux.HandleFunc("/bedtime", func(w http.ResponseWriter, r *http.Request) {
-			assert.Equal(tc, http.MethodGet, r.Method)
-			assert.Equal(tc, "/bedtime?start=2020-01-20", r.URL.String())
-			fmt.Fprint(w, mock)
-		})
+	want := &IdealBedtimes{}
+	json.Unmarshal([]byte(mock), want)
 
-		_, _, err := client.GetBedtime(context.Background(), "2020-01-20", "")
-		assert.NoError(t, err, "should not return an error")
-	})
-
-	t.Run("get bedtime with start and end dates", func(tc *testing.T) {
-		client, mux, _, teardown := setup()
-		defer teardown()
-
-		mux.HandleFunc("/bedtime", func(w http.ResponseWriter, r *http.Request) {
-			assert.Equal(tc, http.MethodGet, r.Method)
-			assert.Equal(tc, "/bedtime?end=2020-01-25&start=2020-01-20", r.URL.String())
-			fmt.Fprint(w, mock)
-		})
-
-		_, _, err := client.GetBedtime(context.Background(), "2020-01-20", "2020-01-25")
-		assert.NoError(t, err, "should not return an error")
-	})
+	assert.ObjectsAreEqual(want, got)
 }
