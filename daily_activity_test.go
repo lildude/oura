135b0ce85f6b0c5ce65a973220d69290fb5a48ba
Colin Seymour <colin@symr.io>
2022-04-03 13:21:48 +0100

    Add mock data

--- daily_activity_test.go
+++ daily_activity_test.go
@@ -24,7 +24,51 @@ var dailyActivityCases = []struct {
 		end_date:    "",
 		next_token:  "",
 		expectedURL: "/v2/usercollection/daily_activity",
-		mock:        `{}`,
+		mock: `{
+			"data": [{
+				"class_5_min": "1111222233334445555",
+				"score": 89,
+				"active_calories": 897,
+				"average_met_minutes": 1.71875,
+				"contributors": {
+					"meet_daily_targets": 100,
+					"move_every_hour": 100,
+					"recovery_time": 69,
+					"stay_active": 86,
+					"training_frequency": 100,
+					"training_volume": 100
+				},
+				"equivalent_walking_distance": 15607,
+				"high_activity_met_minutes": 58,
+				"high_activity_time": 480,
+				"inactivity_alerts": 0,
+				"low_activity_met_minutes": 144,
+				"low_activity_time": 17400,
+				"medium_activity_met_minutes": 525,
+				"medium_activity_time": 7380,
+				"met": {
+					"interval": 60.0,
+					"items": [
+						1.2,
+						10.9
+					],
+					"timestamp": "2022-04-02T04:00:00.000+01:00"
+				},
+				"meters_to_target": -11500,
+				"non_wear_time": 0,
+				"resting_time": 34560,
+				"sedentary_met_minutes": 9,
+				"sedentary_time": 26580,
+				"steps": 12375,
+				"target_calories": 300,
+				"target_meters": 6000,
+				"total_calories": 3012,
+				"day": "2022-04-02",
+				"timestamp": "2022-04-02T04:00:00+01:00"
+			}
+		],
+		"next_token": null
+		}`,
 	},
 	{
 		name:        "get daily activity with only start date",
@@ -32,7 +76,16 @@ var dailyActivityCases = []struct {
 		end_date:    "",
 		next_token:  "",
 		expectedURL: "/v2/usercollection/daily_activity?start_date=2020-01-20",
-		mock:        `{}`,
+		mock: `{
+			"data": [
+				{"timestamp": "2020-01-20T04:00:00+00:00"},
+				{"timestamp": "2020-01-21T04:00:00+00:00"},
+				{"timestamp": "2020-01-22T04:00:00+00:00"},
+				{"timestamp": "2020-01-23T04:00:00+00:00"},
+				{"timestamp": "2020-01-24T04:00:00+00:00"},
+				{"timestamp": "2020-01-25T04:00:00+00:00"},
+				{"timestamp": "2020-01-26T04:00:00+00:00"}
+			]}`,
 	},
 	{
 		name:        "get daily activity with start and end dates",
@@ -40,7 +93,12 @@ var dailyActivityCases = []struct {
 		end_date:    "2020-01-22",
 		next_token:  "",
 		expectedURL: "/v2/usercollection/daily_activity?end_date=2020-01-22&start_date=2020-01-20",
-		mock:        `{}`,
+		mock: `{
+			"data": [
+				{"timestamp": "2020-01-20T04:00:00+00:00"},
+				{"timestamp": "2020-01-21T04:00:00+00:00"},
+				{"timestamp": "2020-01-22T04:00:00+00:00"}
+			]}`,
 	},
 }
 
