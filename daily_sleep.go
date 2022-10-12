47f89409915fde56ecbb9060df57ca39f74a4fde
Colin Seymour <colin@symr.io>
2022-10-12 14:35:51 +0100

    Add v2 DailySleep

---
+++ daily_sleep.go
@@ -0,0 +1,66 @@
+package oura
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+// DailySleep represents the sleep data for a single day.
+type DailySleep struct {
+	Contributors SleepContributors `json:"contributors"`
+	Day          string            `json:"day"`
+	Score        *int              `json:"score,omitempty"`
+	Timestamp    time.Time         `json:"timestamp"`
+}
+
+// DailySleeps represents the sleep data for a given timeframe.
+type DailySleeps struct {
+	Data      []DailySleep `json:"data"`
+	NextToken string       `json:"next_token"`
+}
+
+// Sleep score contributors
+type SleepContributors struct {
+	// Contribution of deep sleep in range [1, 100].
+	DeepSleep *int `json:"deep_sleep"`
+
+	// Contribution of sleep efficiency in range [1, 100].
+	Efficiency *int `json:"efficiency"`
+
+	// Contribution of sleep latency in range [1, 100].
+	Latency *int `json:"latency"`
+
+	// Contribution of REM sleep in range [1, 100].
+	RemSleep *int `json:"rem_sleep"`
+
+	// Contribution of sleep restfulness in range [1, 100].
+	Restfulness *int `json:"restfulness"`
+
+	// Contribution of sleep timing in range [1, 100].
+	Timing *int `json:"timing"`
+
+	// Contribution of total sleep in range [1, 100].
+	TotalSleep *int `json:"total_sleep"`
+}
+
+// DailySleeps gets the daily sleep data for a specified period of time.
+// If a start and end date are not provided, ie are empty strings, we fall back to Oura's defaults which are:
+//
+//	start_date: end_date - 1 day
+//	end_date: current UTC date
+func (c *Client) DailySleeps(ctx context.Context, start_date, end_date, next_token string) (*DailySleeps, *http.Response, error) {
+	path := parametiseDate("/v2/usercollection/daily_sleep", start_date, end_date, next_token)
+	req, err := c.NewRequest("GET", path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var data *DailySleeps
+	resp, err := c.do(ctx, req, &data)
+	if err != nil {
+		return data, resp, err
+	}
+
+	return data, resp, nil
+}
---
+++ daily_sleep_test.go
@@ -0,0 +1,114 @@
+package oura
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var dailySleepCases = []struct {
+	name        string
+	start_date  string
+	end_date    string
+	next_token  string
+	expectedURL string
+	mock        string
+}{
+	{
+		name:        "get daily sleep without specific dates",
+		start_date:  "",
+		end_date:    "",
+		next_token:  "",
+		expectedURL: "/v2/usercollection/daily_sleep",
+		mock: `{
+			"data": [{
+				"contributors": {
+					"deep_sleep": 57,
+					"efficiency": 98,
+					"latency": 81,
+					"rem_sleep": 20,
+					"restfulness": 54,
+					"timing": 84,
+					"total_sleep": 60
+				},
+				"day": "2022-07-14",
+				"score": 63,
+				"timestamp": "2022-07-14T00:00:00+00:00"
+			}],
+			"next_token": null
+		}`,
+	},
+	{
+		name:        "get daily sleep with only start date",
+		start_date:  "2020-01-20",
+		end_date:    "",
+		next_token:  "",
+		expectedURL: "/v2/usercollection/daily_sleep?start_date=2020-01-20",
+		mock:        `{}`, // we don't care about the response here
+	},
+	{
+		name:        "get daily sleep with start and end dates",
+		start_date:  "2020-01-20",
+		end_date:    "2020-01-22",
+		next_token:  "",
+		expectedURL: "/v2/usercollection/daily_sleep?end_date=2020-01-22&start_date=2020-01-20",
+		mock:        `{}`, // we don't care about the response here
+	},
+	{
+		name:        "get daily sleep with next token",
+		start_date:  "",
+		end_date:    "",
+		next_token:  "thisisbase64encodedjson",
+		expectedURL: "/v2/usercollection/daily_sleep?next_token=thisisbase64encodedjson",
+		mock:        `{}`, // We don't care about the response here
+	},
+	{
+		name:        "get daily sleep with start and end dates and next token",
+		start_date:  "2020-01-20",
+		end_date:    "2020-01-22",
+		next_token:  "thisisbase64encodedjson",
+		expectedURL: "/v2/usercollection/daily_sleep?end_date=2020-01-22&next_token=thisisbase64encodedjson&start_date=2020-01-20",
+		mock:        `{}`, // We don't care about the response here
+	},
+	{
+		name:        "get error with dates the wrong way round",
+		start_date:  "2021-10-01",
+		end_date:    "2021-01-01",
+		next_token:  "",
+		expectedURL: "/v2/usercollection/daily_sleep?end_date=2021-01-01&start_date=2021-10-01",
+		mock: `{
+			"detail": "Start time is greater than end time: [start_time: 2021-10-01 01:02:03+00:00; end_date: 2021-01-01 01:02:03+00:00"
+		}`,
+	},
+}
+
+func TestDailySleep(t *testing.T) {
+	for _, tc := range dailySleepCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testDailySleeps(t, tc.start_date, tc.end_date, tc.next_token, tc.expectedURL, tc.mock)
+		})
+	}
+}
+
+func testDailySleeps(t *testing.T, start_date, end_date, next_token, expectedURL, mock string) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/usercollection/daily_sleep", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, expectedURL, r.URL.String())
+		fmt.Fprint(w, mock)
+	})
+
+	got, _, err := client.DailySleeps(context.Background(), start_date, end_date, next_token)
+	assert.NoError(t, err, "should not return an error")
+
+	want := &DailySleeps{}
+	json.Unmarshal([]byte(mock), want) //nolint:errcheck
+
+	assert.ObjectsAreEqual(want, got)
+}
